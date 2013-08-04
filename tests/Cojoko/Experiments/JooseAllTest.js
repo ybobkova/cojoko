3e94014587b011e7b3a8aaea2fe9594b0d39faca
Philipp Scheit <p.scheit@ps-webforge.com>
2013-08-04 17:35:29 +0200

    fix joose all Experiment fix joose reader to get literals and identifiers inkeys of object statement

--- tests/Cojoko/Experiments/JooseAllTest.js
+++ tests/Cojoko/Experiments/JooseAllTest.js
@@ -23,13 +23,20 @@ define(['require',
   'Psc.AjaxFormHandler',
   'Psc.AjaxHandler',
   'Psc.CMS.ComboDropBoxable',
-  'Psc.CMS.FastItem',
   'Psc.CMS.NavigationService',
   'Psc.CMS.TabOpenable',
+  'Psc.CMS.SelectComboBoxable',
+  'Psc.CMS.AutoCompletable',
+  'Psc.CMS.Identifyable',
+  'Psc.CMS.DropBoxButtonable',
+  'Psc.CMS.Buttonable',
+  'Psc.CMS.FastItem',
   'Psc.CalendarCalculation',
   'Psc.CalendarEvent',
   'Psc.Code',
   'Psc.Container',
+  'Psc.ContainerDepending',
+  'Psc.ContainerInjecting',
   'Psc.Date',
   'Psc.EventDispatching',
   'Psc.EventManagerMock',
@@ -56,17 +63,22 @@ define(['require',
   'Psc.UI.ComboDropBox',
   'Psc.UI.ContextMenuManager',
   'Psc.UI.Controller',
+  'Psc.UI.Controlling',
   'Psc.UI.DatePicker',
   'Psc.UI.DateTimePicker',
   'Psc.UI.Dialog',
   'Psc.UI.Dragger',
   'Psc.UI.DropBox',
+  'Psc.UI.DropBoxButton',
   'Psc.UI.EffectsManager',
+  'Psc.UI.EffectsManaging',
   'Psc.UI.ErrorPane',
   'Psc.UI.FormBuilder',
+  'Psc.UI.FormBuilding',
   'Psc.UI.FormController',
   'Psc.UI.FormFields',
   'Psc.UI.FormReader',
+  'Psc.UI.FormReading',
   'Psc.UI.GridPanel',
   'Psc.UI.GridTableEditor',
   'Psc.UI.GridTable',
@@ -75,6 +87,7 @@ define(['require',
   'Psc.UI.HTML.Builder',
   'Psc.UI.HTML.TagBuilder',
   'Psc.UI.InteractionProvider',
+  'Psc.UI.InteractionProviding',
   'Psc.UI.LayoutManager.CollectionWidget',
   'Psc.UI.LayoutManager.ContentStreamWrapper',
   'Psc.UI.LayoutManager.Control',
@@ -86,6 +99,8 @@ define(['require',
   'Psc.UI.LayoutManager.Teaser',
   'Psc.UI.LayoutManager.WebsiteWidget',
   'Psc.UI.LayoutManagerComponent',
+  'Psc.UI.LayoutManager.TemplateWidget',
+  'Psc.UI.TemplateWidgetWrapper',
   'Psc.UI.LayoutManager',
   'Psc.UI.Main',
   'Psc.UI.Menu',
@@ -103,12 +118,14 @@ define(['require',
   'Psc.UI.Table',
   'Psc.UI.Tabs',
   'Psc.UI.Template',
+  'Psc.UI.Translating',
   'Psc.UI.TestInteractionProvider',
   'Psc.UI.UploadableFile',
   'Psc.UI.UploadableImage',
   'Psc.UI.WidgetWrapper',
   'Psc.UI.jqx.GridTableEditor',
   'Psc.UI.jqx.I18nWrapper',
+  'Psc.UI.jqx.WidgetWrapper',
   'Psc.UploadService',
   'Psc.WrongValueException',
   'Psc.ko.Bindings.LayoutManager',
@@ -123,84 +140,37 @@ define(['require',
   'tiptoi.ProgramRunner',
   'tiptoi.SoundImporter',
   'tiptoi.Sound',
+  'tiptoi.StringOutput',
   'tiptoi.Timer',
   'tiptoi.TitoGameEditor',
   'tiptoi.cpu'
     ];
-    var deps = _.map(classes, function (className) { return className.replace(/\./g, '/'); });
-
+    
     var requirePsc = requirejs.config({
       context: "psc-cms-js",
       baseUrl: "/psc-cms-js/lib",
-
- /* set paths and vendor versions for applications
-   *
-   * paths are relative to lib
-   * define all vendor dependencies here
-   */
-  paths: {
-    'jquery': "../vendor/jquery/jquery-1.8.3.min",
-    'jquery-ui': "../vendor/jquery-ui/jquery-ui-1.8.24.custom.patched",
-    'jquery-ui-i18n': "../vendor/jquery-ui/jquery-ui-i18n.custom",
-    "qunit": "../vendor/qunit/qunit-1.10.0",
-    'joose': "../vendor/joose/all",
-    'ace': "../vendor/ace/lib/ace",
-    'lodash': "../vendor/lodash/lodash-0.10.0.min",
-    'psc-tests-assert': '../vendor/qunit-assert/lib/assert',
-    'test-setup': '../tests/setup',
-    'qunit-assert': '../vendor/qunit-assert/lib/assert',
-    'TestRunner': "../vendor/qunit-assert/lib/TestRunner",
-    'img-files': '../img',
-    'jquery-form': "../vendor/jquery-form/jquery.form-3.20",
-    'jquery-fileupload': "../vendor/jquery-fileupload/jquery.fileupload",
-    'jquery-iframe-transport': "../vendor/jquery-fileupload/jquery.iframe-transport",
-    'jquery.ui.widget': "../vendor/jquery-fileupload/vendor/jquery.ui.widget",
-    'jquery-tmpl': "../vendor/jquery-tmpl/jquery.tmpl",
-    'jqwidgets': "../vendor/jqwidgets/jqx-all.min",
-    'jquery-simulate': "../vendor/jquery-simulate/jquery.simulate.patched",
-    'jquery-rangyinputs': "../vendor/jquery-rangyinputs/rangyinputs_jquery.min",
-    'jquerypp': "../vendor/jquerypp/1.0.0/amd/jquerypp",
-    'ui-connect-morphable': "../vendor/webforge/jquery.ui.connect-morphable",
-    'ui-paging': "../vendor/webforge/ui.paging",
-    "JSON": "../vendor/json/json2",
-    "hogan": "../vendor/hogan/hogan-2.0.0.min.amd",
-    'placeholder': "../vendor/mths.be/placeholder-2.0.6",
-    'stacktrace': "../vendor/eriwen/stacktrace-min-0.4",
-    'twitter-bootstrap': "../vendor/twitter-bootstrap/bootstrap",
-    'twitter-typeahead': "../vendor/twitter/typeahead/typeahead.min",
-    'knockout': "../vendor/knockout/knockout-2.2.1",
-    'knockout-mapping': "../vendor/knockout/knockout.mapping",
-    'test-files': "../tests/files",
-    'knockout-bindings': "lib/Psc/ko/bindings",
-    'jquery-selectrange': "../vendor/stackoverflow/jquery-selectrange",
-    'jquery-global': "../vendor/jqwidgets/globalization/jquery.global",
-    'jquery-global-de-DE': "../vendor/jqwidgets/globalization/jquery.glob.de-DE",
-    'templates': "../templates",
-    'html5shiv': "../vendor/afarkas/html5shiv",
-    'i18next': '../vendor/i18next/i18next.amd.withJQuery-1.6.3.min'
-  }
     });
 
-    requirePsc(['require', 'boot-helper'], function (require, boot) {
-      requirePsc(deps, function () {
-        var cojoko;
-        start();
+    var classCodeReader = testSetup.container.getClassCodeReader();
+    classCodeReader.getClassCode = function (fqn) {
+      return requirePsc('text!'+fqn.replace(/\./g, '/')+'.js');
+    };
 
-        _.forEach(classes, function (className) {
-          var jooseClass = window;
-          _.forEach(className.split(/\./), function (part) {
-            jooseClass = jooseClass[part];
-          });
+    var deps = _.map(classes, function (className) { return 'text!'+className.replace(/\./g, '/')+'.js'; });
 
-          cojoko = that.reader.read(jooseClass);
+    requirePsc(deps, function () { // preload deps-sources
+      var cojoko;
+      start();
 
-          that.assertCojoko(cojoko)
-           .name(className)
-          ;
+      _.forEach(classes, function (className) {
+        cojoko = that.reader.read(className, classCodeReader);
 
-          _.forEach(cojoko.reflection.getMethods(), function(method, name) {
-            that.assertContainsNot('$$', method.toString(), 'method '+name+' in '+className+' contains still a $$');
-          });
+        that.assertCojoko(cojoko)
+          .name(className)
+        ;
+
+        _.forEach(cojoko.reflection.getMethods(), function(method, name) {
+          that.assertContainsNot('$$', method.toString(), 'method '+name+' in '+className+' contains still a $$');
         });
       });
     });
